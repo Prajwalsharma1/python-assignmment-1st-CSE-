2569d74e4da5afe688ebf649588ffc0efba91dce
Prajwalsharma1 <97334755+Prajwalsharma1@users.noreply.github.com>
2022-01-14 10:42:55 -0800

    Add files via upload

---
+++ answer.py
@@ -0,0 +1,44 @@
+Microsoft Windows [Version 10.0.19044.1288]
+(c) Microsoft Corporation. All rights reserved.
+
+C:\Users\prajw\Desktop>question.py
+question 1
+enter first number:  34
+enter first number:  23
+enter first number:  54
+avg is: 37.0
+question 2: compute a person's income tax
+Please enter gross income: 100000
+Enter the no. of dependents: 3
+taxable income:
+81000.0
+tax:
+16200.0
+question 3:make a list of SID,NAME,GENDER,COURSE NAME,CGPA
+Please enter name: Prajwal Sharma
+Enter student Id: 21103020
+Enter gender: M
+Enter your course name: CSE
+Enter cgpa: 9.6
+['Name', 'SID', 'Gender', 'Course name', 'Cgpa']
+['Prajwal Sharma ', 21103020, 'M', 'CSE', 9.6]
+question 4: make a list of marks of 5 students and show them in sorted manner
+Enter student 1 marks: 23
+Enter student 2 marks: 45
+Enter student 3 marks: 34
+Enter student 4 marks: 48
+Enter student 5 marks: 50
+List:
+[23, 45, 34, 48, 50]
+Sorted List(decreasing order):
+[50, 48, 45, 34, 23]
+question 5
+(a) print a list and then remove 4th element from the list and let it print the new list
+(b) remove 'Black' and 'Pink' from the list and replace it with 'Purple'
+(a)
+['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+(b)
+['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'Purple', 'Yellow']
+
